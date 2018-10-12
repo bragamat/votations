9790743c5809e49c4ee911c907432f33c55be00f
Mateus Braga <mateuscbraga@gmail.com>
2018-10-12 19:20:06 -0300

    added name to user and a seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ap "================================================================="
+ap "================= DESTROYING ALL USERS  ========================="
+ap "================================================================="
+ap " "
+ap " "
+User.destroy_all
+ap " "
+ap " "
+ap "================================================================="
+ap "============================= DONE =============================="
+ap "================================================================="
+
+ap " "
+ap " "
+ap "================================================================="
+ap "====================== CREATING 4 USERS ========================="
+ap "================================================================="
+
+ap " "
+ap " "
+ap "================================================================="
+ap "====================== CREATING FIRST USER ======================"
+ap "====================== CREATING FIRST USER ======================"
+ap " "
+User.create!(name: "Joka", email: "1_user@vote.com", password: "user_test", password_confirmation: "user_test")
+ap " "
+ap "====================== CREATING SECOND USER ====================="
+ap " "
+User.create!(name: "Fzica", email: "2_user@vote.com", password: "user_test", password_confirmation: "user_test")
+ap " "
+ap "====================== CREATING THIRD USER ======================"
+ap " "
+User.create!(name: "Arthur", email: "3_user@vote.com", password: "user_test", password_confirmation: "user_test")
+ap " "
+ap "====================== CREATING FOURTH USER ====================="
+ap " "
+User.create!(name: "Bragamat", email: "4_user@vote.com", password: "user_test", password_confirmation: "user_test")
+ap " "
+ap "============================= DONE =============================="
